d22c37f33e33c5cd3de671984bf2980dae4b9273
William Chang <william20021026@gmail.com>
2018-11-25 19:18:59 +0800

    rebuild

---
+++ dist/precache-manifest.44def22db995ad265aa382cd65a63bd2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d5217a39d0975c230a0",
+    "url": "/resicolor/dist/css/app.9d7a00a8.css"
+  },
+  {
+    "revision": "6d5217a39d0975c230a0",
+    "url": "/resicolor/dist/js/app.a4125c68.js"
+  },
+  {
+    "revision": "8c195990fb42caabb06b",
+    "url": "/resicolor/dist/css/chunk-vendors.9c8e3772.css"
+  },
+  {
+    "revision": "8c195990fb42caabb06b",
+    "url": "/resicolor/dist/js/chunk-vendors.6bff751b.js"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/resicolor/dist/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/resicolor/dist/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "9ec71506d105bb3e90236e8aefc20186",
+    "url": "/resicolor/dist/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/resicolor/dist/robots.txt"
+  }
+];
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  baseUrl: '/resicolor/dist/',
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
