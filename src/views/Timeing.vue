5d5169c4d50c1f2cfd87afd78dfeb4b440895531
William Chang <william20021026@gmail.com>
2018-11-27 20:54:48 +0800

    add rate

--- src/views/Timeing.vue
+++ src/views/Timeing.vue
@@ -11,8 +11,16 @@
       <h1 v-if="isstart">{{ time }}</h1>
       <Resistor v-if="isstart"/>
       <div v-if="end" class="text item">
+        <el-rate
+          v-model="rate"
+          disabled
+          v-if="end"
+          show-score
+          text-color="#ff9900">
+        </el-rate>
+        <br>
         Total: {{$store.state.total}} <br><br>
-        Avg: {{($store.state.total === 0) ? 0 : (Math.round((avgtime/$store.state.total) * 10) / 10)}} s/one resistor <br><br>
+        Avg: {{($store.state.total === 0) ? 0 : (Math.round((avgtime/$store.state.total) * 10) / 10 + 's/one resistor')}} <br><br>
         <el-button @click="tryagain" round>Try again</el-button>
       </div>
     </el-main>
@@ -33,7 +41,8 @@ export default {
       time: 60,
       avgtime: 60,
       isstart: false,
-      end: false
+      end: false,
+      rate: 5
     }
   },
   components: {
@@ -56,9 +65,32 @@ export default {
           this.timer()
         }, 1000)
       } else {
-        this.isstart = false
-        this.end = true
+        this.timeend()
       }
+    },
+    timeend: function () {
+      this.isstart = false
+      this.end = true
+      let rate = 0
+      let total = this.$store.state.total
+      let avgtime = this.avgtime
+      let avg = (total === 0) ? 0 : (avgtime / total)
+      if (avg < 3) {
+        rate = 5
+      } else if (avg < 5) {
+        rate = 4.5
+      } else if (avg < 6) {
+        rate = 4.2
+      } else if (avg < 8) {
+        rate = 4
+      } else if (avg < 10) {
+        rate = 3
+      } else if (avg < 20) {
+        rate = 2
+      } else if (avg < 30 || avg === 0) {
+        rate = 0
+      }
+      this.rate = rate
     },
     timechange: function (value) {
       this.avgtime = value
