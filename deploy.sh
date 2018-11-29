cbcb9efc35a0794469741940a8aaf8f9c3ef84c4
William Chang <william20021026@gmail.com>
2018-11-29 22:01:45 +0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,15 @@
 npm run test
 npm run build
 cd dist
+mkdir .circleci
+cd .circleci
+touch config.yml
+echo "version: 2\
+jobs:\
+  build:\
+    branches:\
+      only: master" > config.yml
+cd ..
 cp index.html 404.html
 git config --global user.email "wilicw1346@gmail.com"
 git config --global user.name "William Chang (CI)"
