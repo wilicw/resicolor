4e57ab69c0de0f35ec9d8c1ad4d0362d17fb314c
William Chang <william20021026@gmail.com>
2018-12-06 19:26:20 +0800

    use square button keyboard

--- src/components/KeyBoard.vue
+++ src/components/KeyBoard.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="keyboard">
     <el-row :gutter="20">
-      <el-col :span="6"><div><el-button type="info" round @click="inputtext('7')">7</el-button></div></el-col>
-      <el-col :span="6"><div><el-button type="info" round @click="inputtext('8')">8</el-button></div></el-col>
-      <el-col :span="6"><div><el-button type="info" round @click="inputtext('9')">9</el-button></div></el-col>
-      <el-col :span="6"><div><el-button type="info" round @click="inputtext('back')">DEL</el-button></div></el-col>
+      <el-col :span="6"><div><el-button type="info" @click="inputtext('7')">7</el-button></div></el-col>
+      <el-col :span="6"><div><el-button type="info" @click="inputtext('8')">8</el-button></div></el-col>
+      <el-col :span="6"><div><el-button type="info" @click="inputtext('9')">9</el-button></div></el-col>
+      <el-col :span="6"><div><el-button type="info" @click="inputtext('back')">DEL</el-button></div></el-col>
     </el-row>
     <el-row :gutter="20">
-      <el-col :span="6"><div><el-button type="info" round @click="inputtext('4')">4</el-button></div></el-col>
-      <el-col :span="6"><div><el-button type="info" round @click="inputtext('5')">5</el-button></div></el-col>
-      <el-col :span="6"><div><el-button type="info" round @click="inputtext('6')">6</el-button></div></el-col>
-      <el-col :span="6"><div><el-button type="info" round @click="inputtext('M')">M</el-button></div></el-col>
+      <el-col :span="6"><div><el-button type="info" @click="inputtext('4')">4</el-button></div></el-col>
+      <el-col :span="6"><div><el-button type="info" @click="inputtext('5')">5</el-button></div></el-col>
+      <el-col :span="6"><div><el-button type="info" @click="inputtext('6')">6</el-button></div></el-col>
+      <el-col :span="6"><div><el-button type="info" @click="inputtext('M')">M</el-button></div></el-col>
     </el-row>
     <el-row :gutter="20">
-      <el-col :span="6"><div><el-button type="info" round @click="inputtext('1')">1</el-button></div></el-col>
-      <el-col :span="6"><div><el-button type="info" round @click="inputtext('2')">2</el-button></div></el-col>
-      <el-col :span="6"><div><el-button type="info" round @click="inputtext('3')">3</el-button></div></el-col>
-      <el-col :span="6"><div><el-button type="info" round @click="inputtext('K')">K</el-button></div></el-col>
+      <el-col :span="6"><div><el-button type="info" @click="inputtext('1')">1</el-button></div></el-col>
+      <el-col :span="6"><div><el-button type="info" @click="inputtext('2')">2</el-button></div></el-col>
+      <el-col :span="6"><div><el-button type="info" @click="inputtext('3')">3</el-button></div></el-col>
+      <el-col :span="6"><div><el-button type="info" @click="inputtext('K')">K</el-button></div></el-col>
     </el-row>
     <el-row :gutter="20">
-      <el-col :span="6"><div><el-button type="info" round @click="genres()"><i class="el-icon-refresh"></i></el-button></div></el-col>
-      <el-col :span="6"><div><el-button type="info" round @click="inputtext('0')">0</el-button></div></el-col>
-      <el-col :span="6"><div><el-button type="info" round @click="inputtext('.')">.</el-button></div></el-col>
-      <el-col :span="6"><div><el-button type="info" round @click="inputtext('±')">±</el-button></div></el-col>
+      <el-col :span="6"><div><el-button type="info" @click="genres()"><i class="el-icon-refresh"></i></el-button></div></el-col>
+      <el-col :span="6"><div><el-button type="info" @click="inputtext('0')">0</el-button></div></el-col>
+      <el-col :span="6"><div><el-button type="info" @click="inputtext('.')">.</el-button></div></el-col>
+      <el-col :span="6"><div><el-button type="info" @click="inputtext('±')">±</el-button></div></el-col>
     </el-row>
   </div>
 </template>
